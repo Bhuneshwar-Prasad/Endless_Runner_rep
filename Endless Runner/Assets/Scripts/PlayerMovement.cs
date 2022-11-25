c9f0f465e5adeac0ff6c22ca156cc354201964f9
xcloudxgamex <bhuneshwar.prasad1999@gmail.com>
2022-11-25 20:59:38 +0530

    Episode 4
    -added boundary script
    -added player horizontal movement

--- Endless Runner/Assets/Scripts/PlayerMovement.cs
+++ Endless Runner/Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     float player_speed = 3f;
+    float horizontal_speed = 3.5f;
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * player_speed * Time.deltaTime, Space.World);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * horizontal_speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.left * horizontal_speed * -1 * Time.deltaTime);
+        }
     }
 }
---
+++ Endless Runner/Assets/Scripts/playerboundary.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerboundary : MonoBehaviour
+{
+    [SerializeField]
+    float leftboundary;
+    [SerializeField]
+    float rightboundary;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
