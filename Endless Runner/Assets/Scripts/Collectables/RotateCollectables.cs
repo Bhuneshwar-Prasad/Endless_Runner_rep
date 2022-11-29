ed099191f6f7b32e8e92867685b6f1482261eabe
xcloudxgamex <bhuneshwar.prasad1999@gmail.com>
2022-11-29 12:07:59 +0530

    Episode 8
    - added coins
    - added coins in all sections
    - rotated the coin
    - added audio manager

---
+++ Endless Runner/Assets/Scripts/Collectables/RotateCollectables.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCollectables : MonoBehaviour
+{
+    public float rotationSpeed = 3f;
+    void Update()
+    {
+        transform.Rotate(0, rotationSpeed, 0, Space.World);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        FindObjectOfType<AudioManager>().Play("Coin_Sound");
+        this.gameObject.SetActive(false);
+    }
+}
